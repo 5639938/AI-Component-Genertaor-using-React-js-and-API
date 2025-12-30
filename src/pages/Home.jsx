05d2af03c0d6df7a543c436f824241af429a92bd
Anushka  ✨ <anushka.bisney27@gmail.com>
2025-12-30 14:57:58 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
 
 
   const ai = new GoogleGenAI({
-    apiKey:"AIzaSyCGHenQiKgQc45rO1HEjkhoJczqv-EVxrI"
+    apiKey:"YOUR API KEY"
   });
 
   <section class="relative bg-[url('https://images.unsplash.com/photo-1549880338-72b9a791a877?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center h-screen flex items-center justify-center p-4 overflow-hidden">
